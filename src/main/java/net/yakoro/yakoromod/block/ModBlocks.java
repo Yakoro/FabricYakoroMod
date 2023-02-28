00ed895fc3727d99d35a458d0946ee346f405b63
Yanis Pella <yanispella@gmail.com>
2023-02-28 20:33:26 +0100

    Cordierite (block, ore and deepslate_ore)

--- src/main/java/net/yakoro/yakoromod/block/ModBlocks.java
+++ src/main/java/net/yakoro/yakoromod/block/ModBlocks.java
@@ -4,10 +4,12 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
+import net.minecraft.block.OreBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 import net.yakoro.yakoromod.YakoroMod;
 
 import net.minecraft.util.registry.Registry;
@@ -18,9 +20,11 @@ public class ModBlocks {
     public static final Block CORDIERITE_BLOCK = registerBlock("cordierite_block",
             new Block(FabricBlockSettings.of(Material.AMETHYST).strength(4f).requiresTool()), ModItemGroup.CORDIERITE);
     public static final Block CORDIERITE_ORE = registerBlock("cordierite_ore",
-            new Block(FabricBlockSettings.of(Material.AMETHYST).strength(4f).requiresTool()), ModItemGroup.CORDIERITE);
+            new OreBlock(FabricBlockSettings.of(Material.STONE).strength(4f).requiresTool(),
+                    UniformIntProvider.create(1,50)), ModItemGroup.CORDIERITE);
     public static final Block DEEPSLATE_CORDIERITE_ORE = registerBlock("deepslate_cordierite_ore",
-            new Block(FabricBlockSettings.of(Material.AMETHYST).strength(4f).requiresTool()), ModItemGroup.CORDIERITE);
+            new OreBlock(FabricBlockSettings.of(Material.STONE).strength(4f).requiresTool(),
+            UniformIntProvider.create(1,50)), ModItemGroup.CORDIERITE);
 
     private static Block registerBlock(String name, Block block, ItemGroup tab){
         registerBlockItem(name, block, tab);
