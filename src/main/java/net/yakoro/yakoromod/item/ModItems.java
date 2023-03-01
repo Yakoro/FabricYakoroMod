386e14cc141481b0f9362956f59d5125d18e9277
LENOVO <yanispella@gmail.com>
2023-03-01 14:09:59 +0100

    Cordierite Sword

--- src/main/java/net/yakoro/yakoromod/item/ModItems.java
+++ src/main/java/net/yakoro/yakoromod/item/ModItems.java
@@ -3,9 +3,11 @@ package net.yakoro.yakoromod.item;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.SwordItem;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.yakoro.yakoromod.YakoroMod;
+import net.yakoro.yakoromod.item.toolmaterial.CordieriteToolMaterial;
 
 public class ModItems {
 
@@ -14,6 +16,8 @@ public class ModItems {
     public static final Item CORDIERITE = registerItem("cordierite",
             new Item(new FabricItemSettings().group(ItemGroup.MISC).group(ModItemGroup.CORDIERITE)));
 
+    public static final Item CORDIERITE_SWORD = registerItem("cordierite_sword",
+            new SwordItem(CordieriteToolMaterial.INSTANCE, 20, -2f, new FabricItemSettings().group(ItemGroup.MISC).group(ModItemGroup.CORDIERITE)));
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(YakoroMod.MOD_ID, name), item);
     }
---
+++ src/main/java/net/yakoro/yakoromod/item/toolmaterial/custom/KnockbackHealStickItem.java
@@ -0,0 +1,4 @@
+package net.yakoro.yakoromod.item.toolmaterial.custom;
+
+public class KnockbackHealStickItem {
+}
