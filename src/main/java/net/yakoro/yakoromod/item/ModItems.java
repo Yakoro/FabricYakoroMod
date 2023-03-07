f636344217841e16c1eadbf1a72aa3e781562e29
Yakoro <yanispella@gmail.com>
2023-03-08 00:50:13 +0100

    Totem of God

--- src/main/java/net/yakoro/yakoromod/item/ModItems.java
+++ src/main/java/net/yakoro/yakoromod/item/ModItems.java
@@ -1,10 +1,6 @@
 package net.yakoro.yakoromod.item;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.fabricmc.fabric.impl.object.builder.FabricEntityType;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.*;
@@ -12,8 +8,8 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.yakoro.yakoromod.YakoroMod;
 import net.yakoro.yakoromod.block.ModBlocks;
-import net.yakoro.yakoromod.entity.custom.ExplosiveProjectileEntity;
 import net.yakoro.yakoromod.item.custom.ExplosiveGunItem;
+import net.yakoro.yakoromod.item.custom.TotemOfGodItem;
 import net.yakoro.yakoromod.item.toolmaterial.CordieriteToolMaterialPickaxe;
 import net.yakoro.yakoromod.item.toolmaterial.CordieriteToolMaterialSword;
 import net.yakoro.yakoromod.item.custom.HealingStaffItem;
@@ -50,6 +46,9 @@ public class ModItems {
 
     public static final Item HEARTBEETROOTS = registerItem("heartbeetroots",
             new Item(new FabricItemSettings().group(ItemGroup.MISC).group(ModItemGroup.CORDIERITE).food(new FoodComponent.Builder().hunger(2).saturationModifier(4f).statusEffect(new StatusEffectInstance(StatusEffects.INSTANT_HEALTH, 1, 1) , 1.0f).build())));
+
+    public static final Item TOTEM_OF_GOD = registerItem("totem_of_god",
+            new TotemOfGodItem(new FabricItemSettings().group(ItemGroup.MISC).group(ModItemGroup.CORDIERITE)));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(YakoroMod.MOD_ID, name), item);
---
+++ src/main/java/net/yakoro/yakoromod/item/custom/TotemOfGodItem.java
@@ -0,0 +1,40 @@
+package net.yakoro.yakoromod.item.custom;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.world.World;
+import net.minecraft.server.MinecraftServer;
+
+public class TotemOfGodItem extends Item {
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+
+        if (!user.world.isClient() && hand == Hand.MAIN_HAND) {
+            if (world.isNight()) {
+                MinecraftServer server = world.getServer();
+                if (server != null) {
+                    long timeOfDay = 6000; // midi
+                    server.getCommandManager().execute(server.getCommandSource().withSilent().withLevel(2),
+                            "time set " + timeOfDay);
+                }
+            } else {
+                MinecraftServer server = world.getServer();
+                if (server != null) {
+                    long timeOfDay = 13000; // minuit
+                    server.getCommandManager().execute(server.getCommandSource().withSilent().withLevel(2),
+                            "time set " + timeOfDay);
+                }
+            }
+
+        }
+        return super.use(world, user, hand);
+    }
+
+    public TotemOfGodItem(Settings settings) {
+        super(settings);
+    }
+}
