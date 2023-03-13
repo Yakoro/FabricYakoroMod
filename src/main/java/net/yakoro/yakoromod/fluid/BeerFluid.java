181ebd1e7261c8d994c48c4e64efd7fd2ba77d69
LENOVO <yanispella@gmail.com>
2023-03-13 13:41:40 +0100

    Better Fluids

--- src/main/java/net/yakoro/yakoromod/fluid/BeerFluid.java
+++ src/main/java/net/yakoro/yakoromod/fluid/BeerFluid.java
@@ -7,13 +7,20 @@ import net.minecraft.fluid.FlowableFluid;
 import net.minecraft.fluid.Fluid;
 import net.minecraft.fluid.FluidState;
 import net.minecraft.item.Item;
+import net.minecraft.particle.ParticleEffect;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.random.Random;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
 import net.minecraft.world.WorldAccess;
 import net.minecraft.world.WorldView;
+import org.jetbrains.annotations.Nullable;
 
 public abstract class BeerFluid extends FlowableFluid {
 
@@ -28,6 +35,28 @@ public abstract class BeerFluid extends FlowableFluid {
         Block.dropStacks(state, world, pos, blockEntity);
     }
 
+    @Override
+    public void randomDisplayTick(World world, BlockPos pos, FluidState state, Random random) {
+        BlockPos blockPos = pos.up();
+        if (world.getBlockState(blockPos).isAir() && !world.getBlockState(blockPos).isOpaqueFullCube(world, blockPos)) {
+            if (random.nextInt(20) == 0) {
+                double d = (double)pos.getX() + random.nextDouble();
+                double e = (double)pos.getY() + 1.0;
+                double f = (double)pos.getZ() + random.nextDouble();
+                world.addParticle(ParticleTypes.CLOUD, d, e, f, 0.0, 0.0, 0.0);
+                world.playSound(d, e, f, SoundEvents.AMBIENT_UNDERWATER_EXIT, SoundCategory.BLOCKS, 0.2f + random.nextFloat() * 0.2f, 0.9f + random.nextFloat() * 0.15f, false);
+            }
+            if (random.nextInt(200) == 0) {
+                world.playSound(pos.getX(), (double)pos.getY(), (double)pos.getZ(), SoundEvents.BLOCK_LAVA_AMBIENT, SoundCategory.BLOCKS, 0.2f + random.nextFloat() * 0.2f, 0.9f + random.nextFloat() * 0.15f, false);
+            }
+        }
+    }
+
+    @Override
+    @Nullable
+    public ParticleEffect getParticle() {
+        return ParticleTypes.BUBBLE;
+    }
     @Override
     protected int getFlowSpeed(WorldView world) { return 0; }
 
